b477791d40b176d6e1a9e76bcf6eb49556ad9f07
Shivam Kislay <shivam.kislay@iloy-group.com>
2020-10-14 16:23:32 +0100

    Add project files.

---
+++ ConsumerApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+
+namespace ConsumerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string topic = "TestTopic";
+            Uri uri = new Uri("http://localhost:9092");
+            var options = new KafkaOptions(uri);
+            var router = new BrokerRouter(options);
+            var consumer = new Consumer(new ConsumerOptions(topic, router));
+            foreach (var message in consumer.Consume())
+            {
+                Console.WriteLine(Encoding.UTF8.GetString(message.Value));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProducerApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+namespace ProducerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string topic = "TestTopic";
+            Console.WriteLine("Enter the message to pass");
+            string payload = Console.ReadLine();
+            Message msg = new Message(payload);
+            Uri uri = new Uri("http://localhost:9092");
+            var options = new KafkaOptions(uri);
+            var router = new BrokerRouter(options);
+            var client = new Producer(router);
+            client.SendMessageAsync(topic, new List<Message> { msg }).Wait();
+            Console.ReadLine();
+        }
+    }
+}
